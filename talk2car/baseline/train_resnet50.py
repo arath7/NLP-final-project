4aa74b1f032b60488072a5bc441ce836246754ed
Ananya Rath <60801863+arath7@users.noreply.github.com>
2025-11-30 01:24:37 -0500

    pushing resnet50 code and original train.py code

--- talk2car/baseline/train_resnet50.py
+++ talk2car/baseline/train_resnet50.py
@@ -29,9 +29,9 @@ parser.add_argument('--epochs', default=10, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
-parser.add_argument('-b', '--batch-size', default=18, type=int,
+parser.add_argument('-b', '--batch-size', default=2, type=int,
                     metavar='N',
-                    help='mini-batch size (default: 18)')
+                    help='mini-batch size (default: 2)')
 parser.add_argument('--lr', '--learning-rate', default=0.01, type=float,
                     metavar='LR', help='initial learning rate', dest='lr')
 parser.add_argument('--milestones', default=[4, 8], nargs='*', type=int,
@@ -51,7 +51,6 @@ parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
 def main():
     args = parser.parse_args()
 
-    # Create dataset
     print("=> creating dataset")
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                   std=[0.229, 0.224, 0.225])
@@ -61,17 +60,19 @@ def main():
                         transform=transforms.Compose([transforms.ToTensor(), normalize]))
 
     train_dataloader = data.DataLoader(train_dataset, batch_size = args.batch_size, shuffle=True,
-                            num_workers=args.workers, collate_fn=custom_collate, pin_memory=True,                            drop_last=True)
+                            num_workers=args.workers, collate_fn=custom_collate, pin_memory=True, drop_last=True)
     val_dataloader = data.DataLoader(val_dataset, batch_size = args.batch_size, shuffle=False,
-                            num_workers=args.workers, collate_fn=custom_collate, pin_memory=True,                            drop_last=False)
+                            num_workers=args.workers, collate_fn=custom_collate, pin_memory=True, drop_last=False)
 
-    # Create model
     print("=> creating model")
-    img_encoder = resnet.__dict__['resnet18'](pretrained=True) 
+    img_encoder = resnet.__dict__['resnet50'](pretrained=True) 
     text_encoder = nlp_models.TextEncoder(input_dim=train_dataset.number_of_words(),
                                                  hidden_size=512, dropout=0.1)                          
     img_encoder.cuda()
     text_encoder.cuda()
+
+    img_projection = nn.Linear(2048, 512)
+    img_projection.cuda()
 
     criterion = nn.CrossEntropyLoss(ignore_index = train_dataset.ignore_index, 
                                     reduction = 'mean')
@@ -79,21 +80,21 @@ def main():
     
     cudnn.benchmark = True
 
-    # Optimizer and scheduler
     print("=> creating optimizer and scheduler")
-    params = list(text_encoder.parameters()) + list(img_encoder.parameters())
+    params = list(text_encoder.parameters()) + list(img_encoder.parameters()) + list(img_projection.parameters())
     optimizer = optim.SGD(params, lr=args.lr, momentum=args.momentum, 
                             weight_decay=args.weight_decay, nesterov=args.nesterov)
     scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=args.milestones,
                             gamma=0.1)
 
-    # Checkpoint
     checkpoint = 'checkpoint.pth.tar'
     if os.path.exists(checkpoint):
         print("=> resume from checkpoint at %s" %(checkpoint))
         checkpoint = torch.load(checkpoint, map_location='cpu')
         img_encoder.load_state_dict(checkpoint['img_encoder'])
         text_encoder.load_state_dict(checkpoint['text_encoder'])
+        if 'img_projection' in checkpoint:
+            img_projection.load_state_dict(checkpoint['img_projection'])
         optimizer.load_state_dict(checkpoint['optimizer'])
         scheduler.load_state_dict(checkpoint['scheduler'])
         start_epoch = checkpoint['epoch']
@@ -103,23 +104,18 @@ def main():
         best_ap50 = 0
         start_epoch = 0
 
-    # Start training
     print("=> start training")
 
     for epoch in range(start_epoch, args.epochs):
         print('Start epoch %d/%d' %(epoch, args.epochs))
         print(20*'-')
 
-        # Train
-        train(train_dataloader, img_encoder, text_encoder, optimizer, criterion, epoch, args)
+        train(train_dataloader, img_encoder, text_encoder, img_projection, optimizer, criterion, epoch, args)
         
-        # Update lr rate
         scheduler.step()
         
-        # Evaluate
-        ap50 = evaluate(val_dataloader, img_encoder, text_encoder, args)
+        ap50 = evaluate(val_dataloader, img_encoder, text_encoder, img_projection, args)
         
-        # Checkpoint
         if ap50 > best_ap50:
             new_best = True
             best_ap50 = ap50
@@ -128,38 +124,37 @@ def main():
 
         save_checkpoint({'img_encoder': img_encoder.state_dict(),
                          'text_encoder': text_encoder.state_dict(),
+                         'img_projection': img_projection.state_dict(),
                          'optimizer': optimizer.state_dict(),
                          'scheduler': scheduler.state_dict(),
                          'epoch': epoch + 1, 'best_ap50': best_ap50}, new_best = new_best)
 
-    # Evaluate
     if args.evaluate:
         print("=> Evaluating best model")
         checkpoint = torch.load('best_model.pth.tar', map_location='cpu')
         img_encoder.load_state_dict(checkpoint['img_encoder'])
         text_encoder.load_state_dict(checkpoint['text_encoder'])
-        ap50 = evaluate(val_dataloader, img_encoder, text_encoder, args)
+        if 'img_projection' in checkpoint:
+            img_projection.load_state_dict(checkpoint['img_projection'])
+        ap50 = evaluate(val_dataloader, img_encoder, text_encoder, img_projection, args)
         print('AP50 on validation set is %.2f' %(ap50*100))
 
-def train(train_dataloader, img_encoder, text_encoder, optimizer, criterion,
-            epoch, args):
+def train(train_dataloader, img_encoder, text_encoder, img_projection, optimizer, criterion, epoch, args):
     m_losses = AverageMeter('Loss', ':.4e')
     m_top1 = AverageMeter('Acc@1', ':6.2f')
     m_iou = AverageMeter('IoU', ':6.2f')
     m_ap50 = AverageMeter('AP50', ':6.2f')
-    progress = ProgressMeter(
-                len(train_dataloader),
-                [m_losses, m_top1, m_iou, m_ap50], prefix="Epoch: [{}]".format(epoch))
+    progress = ProgressMeter(len(train_dataloader), [m_losses, m_top1, m_iou, m_ap50], prefix="Epoch: [{}]".format(epoch))
  
     img_encoder.train()
     text_encoder.train()
+    img_projection.train()
     
     ignore_index = train_dataloader.dataset.ignore_index
         
     for i, batch in enumerate(train_dataloader):
         optimizer.zero_grad()
         
-        # Data
         region_proposals = batch['rpn_image'].cuda(non_blocking=True)
         command = batch['command'].cuda(non_blocking=True)
         command_length = batch['command_length'].cuda(non_blocking=True)
@@ -167,28 +162,23 @@ def train(train_dataloader, img_encoder, text_encoder, optimizer, criterion,
         iou = batch['rpn_iou'].cuda(non_blocking=True)
         b, r, c, h, w = region_proposals.size()
 
-        # Image features
         img_features = img_encoder(region_proposals.view(b*r, c, h, w))
+        img_features = img_projection(img_features)
         norm = img_features.norm(p=2, dim=1, keepdim=True)
         img_features = img_features.div(norm)
        
-        # Sentence features
         _, sentence_features = text_encoder(command.permute(1,0), command_length)
         norm = sentence_features.norm(p=2, dim=1, keepdim=True)
         sentence_features = sentence_features.div(norm)
      
-        # Product in latent space
         scores = torch.bmm(img_features.view(b, r, -1), sentence_features.unsqueeze(2)).squeeze()
 
-        # Loss
         total_loss = criterion(scores, gt)
 
-        # Update
         optimizer.zero_grad()
         total_loss.backward()
         optimizer.step()
         
-        # Summary
         pred = torch.argmax(scores, 1)
         pred_bin = F.one_hot(pred, r).bool()
         valid = (gt!=ignore_index)
@@ -203,7 +193,7 @@ def train(train_dataloader, img_encoder, text_encoder, optimizer, criterion,
     
 
 @torch.no_grad()
-def evaluate(val_dataloader, img_encoder, text_encoder, args):
+def evaluate(val_dataloader, img_encoder, text_encoder, img_projection, args):
     m_top1 = AverageMeter('Acc@1', ':6.2f')
     m_iou = AverageMeter('IoU', ':6.2f')
     m_ap50 = AverageMeter('AP50', ':6.2f')
@@ -214,12 +204,12 @@ def evaluate(val_dataloader, img_encoder, text_encoder, args):
  
     img_encoder.eval()
     text_encoder.eval()
+    img_projection.eval()
     
     ignore_index = val_dataloader.dataset.ignore_index
  
     for i, batch in enumerate(val_dataloader):
         
-        # Data
         region_proposals = batch['rpn_image'].cuda(non_blocking=True)
         command = batch['command'].cuda(non_blocking=True)
         command_length = batch['command_length'].cuda(non_blocking=True)
@@ -230,23 +220,20 @@ def evaluate(val_dataloader, img_encoder, text_encoder, args):
 
         b, r, c, h, w = region_proposals.size()
 
-        # Image features
         img_features = img_encoder(region_proposals.view(b*r, c, h, w))
+        img_features = img_projection(img_features)
         norm = img_features.norm(p=2, dim=1, keepdim=True)
         img_features = img_features.div(norm)
        
-        # Sentence features
         _, sentence_features = text_encoder(command.permute(1,0), command_length)
         norm = sentence_features.norm(p=2, dim=1, keepdim=True)
         sentence_features = sentence_features.div(norm)
      
-        # Product in latent space
         scores = torch.bmm(img_features.view(b, r, -1), sentence_features.unsqueeze(2)).squeeze()
 
         if len(batch["index"].shape) == 0:
             scores = scores.unsqueeze(0)
 
-        # Summary
         pred = torch.argmax(scores, 1)
         gt = batch['rpn_gt'].cuda(non_blocking=True)
         iou = batch['rpn_iou'].cuda(non_blocking=True)
